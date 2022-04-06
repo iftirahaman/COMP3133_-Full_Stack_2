ccea33d55016d865254a661df719b26661b73a6b
Sheak Iftakhar Rahaman <sheakiftakhar.rahaman@georgebrown.ca>
2022-04-05 21:03:20 -0400

    COMP3133_-Full_Stack_2_Winter_2022

---
+++ 101054615-lab-test2-comp3133/src/app/missionlist/missionlist.component.ts
@@ -0,0 +1,35 @@
+import { Component, NgModule, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Mission } from '../models/mission';
+import { SpacexService } from '../network/spacex.service';
+import { Router } from '@angular/router';
+import {MatCardModule} from '@angular/material/card';
+@Component({
+  selector: 'app-missionlist',
+  templateUrl: './missionlist.component.html',
+  styleUrls: ['./missionlist.component.css']
+})
+
+export class MissionlistComponent implements OnInit {
+ 
+  dataList: Mission[]= [];
+ 
+  constructor(private spacexservice: SpacexService) { }
+
+  ngOnInit() {
+   this.getMissionList();
+   
+  }
+  getMissionList():void{
+    
+    this.spacexservice.getMission().subscribe((res)=>{
+      console.log(res);
+     this.dataList = res;
+      
+      
+      
+    }); 
+  }
+
+  
+}
